e7e777f4e011de8fcf40688c21ec1849d7b90309
ORMIERES Lola <ormieresl@sxrecyf2>
2023-09-12 15:33:10 +0200

    Some folders added

---
+++ colorbar/userconfig.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#epsilon = 1e-12
+#mask_outside=1e17
+# usermodules = [{'name':'module1', 'abspath':'/path/to/module1.py'},
+#                {'name':'module2', 'abspath':'/path/to/module2.py'}
+#                ]
+#use_footprints_as_builder = True
+#usercolormaps = {'my_cmap':'/path/to/my_cmap.cmap'}
+#implemented_formats = ['FA',]
+noninteractive_backend = "Agg"
+usercolormaps={'biais': '/home/ormieresl/.epygram/palettes/biais.json',
+        'biais_v1': '/home/ormieresl/.epygram/palettes/biais_v1.json',
+        'biais_v4': '/home/ormieresl/.epygram/palettes/biais_v4.json',
+        'biais_v4_grey': '/home/ormieresl/.epygram/palettes/biais_v4_grey_v2.json',
+        'biais_v4_grey_v3': '/home/ormieresl/.epygram/palettes/biais_v4_grey_v3.json',
+        'biais_sst_cyclone': '/home/ormieresl/.epygram/palettes/biais_sst_cyclone.json',
+        'biais_sst_cyclone_thiner': '/home/ormieresl/.epygram/palettes/biais_sst_cyclone_thiner.json',
+        'biais_sst_cyclone_restricted': '/home/ormieresl/.epygram/palettes/biais_sst_cyclone_restricted.json',
+        'biais_sst_cyclone_large': '/home/ormieresl/.epygram/palettes/biais_sst_cyclone_large.json',
+        'biais_sst': '/home/ormieresl/.epygram/palettes/biais_sst.json',
+        'biais_sst_v1': '/home/ormieresl/.epygram/palettes/biais_sst_v1.json',
+        'biais_lat': '/home/ormieresl/.epygram/palettes/biais_lat.json',
+        'biais_flx_lat': '/home/ormieresl/.epygram/palettes/biais_flx_lat.json',
+        'biais_lat_diff': '/home/ormieresl/.epygram/palettes/biais_lat_diff.json',
+        'biais_lat_thiner': '/home/ormieresl/.epygram/palettes/biais_lat_thiner.json',
+        'undef': '/home/ormieresl/.epygram/palettes/undef.json',
+        'std':'/home/ormieresl/.epygram/palettes/std.json',
+        'biais_temp_scores':'/home/ormieresl/.epygram/palettes/biais_temp_scores.json' }                                                                               
